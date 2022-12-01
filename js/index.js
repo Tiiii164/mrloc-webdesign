6dcfeb2257804e9352f68496047f3662b7f05888
Binh Pham <binhptgcs200187@fpt.edu.vn>
2022-12-02 02:26:14 +0700

    video 5-6

--- js/index.js
+++ js/index.js
@@ -1,17 +1,53 @@
 const headerElement = document.querySelector(".header");
 document.addEventListener("scroll", function (e) {
-   let currentPos = document.documentElement.scrollTop;
-   if(currentPos >= 120 ){
-    headerElement.classList.add("fixed");
-   }
-   else{
-    headerElement.classList.remove("fixed");
-   }
+    let currentPos = document.documentElement.scrollTop;
+    if (currentPos >= 120) {
+        headerElement.classList.add("fixed");
+    }
+    else {
+        headerElement.classList.remove("fixed");
+    }
 })
-    // menu slidebar 
+// menu slidebar 
 const itemSliderBar = document.querySelectorAll(".category-left-li");
-itemSliderBar.forEach((menu,index)=>{
-        menu.addEventListener('click',(e)=>{
-            menu.classList.toggle("block");
-        })
+itemSliderBar.forEach((menu, index) => {
+    menu.addEventListener('click', (e) => {
+        menu.classList.toggle("block");
+    })
 })
+
+//products
+const bigimg = document.querySelector(".product-content-left-big-img img")
+const smallimg = document.querySelectorAll(".product-content-left-small-img img")
+smallimg.forEach(function (imgitem, X) {
+    imgitem.addEventListener("click", function () {
+        bigimg.src = imgitem.src
+    })
+})
+
+
+
+
+const baoquan = document.querySelector(".baoquan")
+const chitiet = document.querySelector(".chitiet")
+
+if (baoquan) {
+    baoquan.addEventListener("click", function () {
+        document.querySelector(".product-content-right-bottom-content-chitiet").style.display = "none"
+        document.querySelector(".product-content-right-bottom-content-baoquan").style.display = "block"
+    })
+}
+
+if (chitiet) {
+    chitiet.addEventListener("click", function () {
+        document.querySelector(".product-content-right-bottom-content-chitiet").style.display = "block"
+        document.querySelector(".product-content-right-bottom-content-baoquan").style.display = "none"
+    })
+}
+
+const button = document.querySelector(".product-content-right-bottom-top")
+if (button) {
+    button.addEventListener("click", function () {
+        document.querySelector(".product-content-right-bottom-content-big").classList.toggle("activeB")
+    })
+}
